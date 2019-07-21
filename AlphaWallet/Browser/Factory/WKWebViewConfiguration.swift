9d0925f4097636ddf30255e51f664ba3a6266722
James Sangalli <j.l.sangalli@gmail.com>
2019-07-21 18:52:06 +1000

    add async hooks

--- AlphaWallet/Browser/Factory/WKWebViewConfiguration.swift
+++ AlphaWallet/Browser/Factory/WKWebViewConfiguration.swift
@@ -117,7 +117,209 @@ extension WKWebViewConfiguration {
                           //send back the coinbase account as an array of one
                           resolve([addressHex])
                       })
-                   }
+                   },
+                   send: function(payload) {
+                      let response = {
+                        jsonrpc: "2.0",
+                        id: payload.id
+                      };
+                      switch(payload.method) {
+                        case "eth_accounts":
+                          response.result = this.eth_accounts();
+                          break;
+                        case "eth_coinbase":
+                          response.result = this.eth_coinbase();
+                          break;
+                        case "net_version":
+                          response.result = this.net_version();
+                          break;
+                        case "eth_uninstallFilter":
+                          this.sendAsync(payload, (error) => {
+                            if (error) {
+                              console.log(`<== uninstallFilter ${error}`);
+                            }
+                          });
+                          response.result = true;
+                          break;
+                        default:
+                          throw new Error(`AlphaWallet does not support calling ${payload.method} synchronously without a callback. Please provide a callback parameter to call ${payload.method} asynchronously.`);
+                      }
+                      return response;
+                    },
+
+                    sendAsync: function(payload, callback) {
+                      if (Array.isArray(payload)) {
+                        Promise.all(payload.map(this._sendAsync.bind(this)))
+                        .then(data => callback(null, data))
+                        .catch(error => callback(error, null));
+                      } else {
+                        this._sendAsync(payload)
+                        .then(data => callback(null, data))
+                        .catch(error => callback(error, null));
+                      }
+                    },
+
+                    _sendAsync: function(payload) {
+                      this.idMapping.tryIntifyId(payload);
+                      return new Promise((resolve, reject) => {
+                        if (!payload.id) {
+                          payload.id = Utils.genId();
+                        }
+                        this.callbacks.set(payload.id, (error, data) => {
+                          if (error) {
+                            reject(error);
+                          } else {
+                            resolve(data);
+                          }
+                        });
+
+                        switch(payload.method) {
+                          case "eth_accounts":
+                            return this.sendResponse(payload.id, this.eth_accounts());
+                          case "eth_coinbase":
+                            return this.sendResponse(payload.id, this.eth_coinbase());
+                          case "net_version":
+                            return this.sendResponse(payload.id, this.net_version());
+                          case "eth_sign":
+                            return this.eth_sign(payload);
+                          case "personal_sign":
+                            return this.personal_sign(payload);
+                          case "personal_ecRecover":
+                            return this.personal_ecRecover(payload);
+                          case "eth_signTypedData":
+                          case "eth_signTypedData_v3":
+                            return this.eth_signTypedData(payload);
+                          case "eth_sendTransaction":
+                            return this.eth_sendTransaction(payload);
+                          case "eth_requestAccounts":
+                            return this.eth_requestAccounts(payload);
+                          case "eth_newFilter":
+                            return this.eth_newFilter(payload);
+                          case "eth_newBlockFilter":
+                            return this.eth_newBlockFilter(payload);
+                          case "eth_newPendingTransactionFilter":
+                            return this.eth_newPendingTransactionFilter(payload);
+                          case "eth_uninstallFilter":
+                            return this.eth_uninstallFilter(payload);
+                          case "eth_getFilterChanges":
+                            return this.eth_getFilterChanges(payload);
+                          case "eth_getFilterLogs":
+                            return this.eth_getFilterLogs(payload);
+                          default:
+                            this.callbacks.delete(payload.id);
+                            return this.rpc.call(payload).then(resolve).catch(reject);
+                        }
+                      });
+                    },
+
+                    eth_accounts: function() {
+                      return this.address ? [this.address] : [];
+                    },
+
+                    eth_coinbase: function() {
+                      return this.address;
+                    },
+
+                    net_version: function() {
+                      return this.chainId.toString(10) || null;
+                    },
+
+                    eth_sign: function(payload) {
+                      this.postMessage("signMessage", payload.id, {data: payload.params[1]});
+                    },
+
+                    personal_sign: function(payload) {
+                      this.postMessage("signPersonalMessage", payload.id, {data: payload.params[0]});
+                    },
+
+                    personal_ecRecover: function(payload) {
+                      this.postMessage("ecRecover", payload.id, {signature: payload.params[1], message: payload.params[0]});
+                    },
+
+                    eth_signTypedData: function(payload) {
+                      this.postMessage("signTypedMessage", payload.id, {data: payload.params[1]});
+                    },
+
+                    eth_sendTransaction: function(payload) {
+                      this.postMessage("signTransaction", payload.id, payload.params[0]);
+                    },
+
+                    eth_requestAccounts: function(payload) {
+                      this.postMessage("requestAccounts", payload.id, {});
+                    },
+
+                    eth_newFilter: function(payload) {
+                      this.filterMgr.newFilter(payload)
+                      .then(filterId => this.sendResponse(payload.id, filterId))
+                      .catch(error => this.sendError(payload.id, error));
+                    },
+
+                    eth_newBlockFilter: function(payload) {
+                      this.filterMgr.newBlockFilter()
+                      .then(filterId => this.sendResponse(payload.id, filterId))
+                      .catch(error => this.sendError(payload.id, error));
+                    },
+
+                    eth_newPendingTransactionFilter: function(payload) {
+                      this.filterMgr.newPendingTransactionFilter()
+                      .then(filterId => this.sendResponse(payload.id, filterId))
+                      .catch(error => this.sendError(payload.id, error));
+                    },
+
+                    eth_uninstallFilter: function(payload) {
+                      this.filterMgr.uninstallFilter(payload.params[0])
+                      .then(filterId => this.sendResponse(payload.id, filterId))
+                      .catch(error => this.sendError(payload.id, error));
+                    },
+
+                    eth_getFilterChanges: function(payload) {
+                      this.filterMgr.getFilterChanges(payload.params[0])
+                      .then(data => this.sendResponse(payload.id, data))
+                      .catch(error => this.sendError(payload.id, error));
+                    },
+
+                    eth_getFilterLogs: function(payload) {
+                      this.filterMgr.getFilterLogs(payload.params[0])
+                      .then(data => this.sendResponse(payload.id, data))
+                      .catch(error => this.sendError(payload.id, error));
+                    },
+
+                    postMessage: function(handler, id, data) {
+                      if (this.ready || handler === "requestAccounts") {
+                        window.webkit.messageHandlers[handler].postMessage({
+                          "name": handler,
+                          "object": data,
+                          "id": id
+                        });
+                      } else {
+                        // don't forget to verify in the app
+                        this.sendError(id, new Error("provider is not ready"));
+                      }
+                    },
+
+                    sendResponse: function(id, result) {
+                      let originId = this.idMapping.tryPopId(id) || id;
+                      let callback = this.callbacks.get(id);
+                      let data = {jsonrpc: "2.0", id: originId};
+                      if (typeof result === "object" && result.jsonrpc && result.result) {
+                        data.result = result.result;
+                      } else {
+                        data.result = result;
+                      }
+                      if (callback) {
+                        callback(null, data);
+                        this.callbacks.delete(id);
+                      }
+                    },
+
+                    sendError: function(id, error) {
+                      console.log(`<== ${id} sendError ${error}`);
+                      let callback = this.callbacks.get(id);
+                      if (callback) {
+                        callback(error instanceof Error ? error : new Error(error), null);
+                        this.callbacks.delete(id);
+                      }
+                    }
                }, {
                    address: addressHex,
                    networkVersion: chainID
